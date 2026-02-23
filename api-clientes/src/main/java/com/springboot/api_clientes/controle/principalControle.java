4ddf77972baba1b1fcad8b6c5526a219392f60cc
deividandrade <deividandrade.br@gmail.com>
2026-02-22 21:15:08 -0300

    first commit

---
+++ api-clientes/src/main/java/com/springboot/api_clientes/controle/principalControle.java
@@ -0,0 +1,14 @@
+package com.springboot.api_clientes.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class principalControle {
+
+	@GetMapping("/administrativo")
+	public String acessarPrincipal() {
+		return "administrativo/home";
+	}
+	
+}
