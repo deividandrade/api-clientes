07fa67b3cbda33c24a60b0e4fb07949968091002
deividandrade <deividandrade.br@gmail.com>
2026-02-23 21:26:54 -0300

    lista - editar e remover

--- api-clientes/src/main/java/com/springboot/api_clientes/controle/EstadoControle.java
+++ api-clientes/src/main/java/com/springboot/api_clientes/controle/EstadoControle.java
@@ -1,9 +1,12 @@
 package com.springboot.api_clientes.controle;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,6 +25,31 @@ public class EstadoControle {
 		mv.addObject("estado", estado);
 		return mv;
 	}
+	
+	@GetMapping("/listarEstado")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("administrativo/estados/lista");
+		mv.addObject("listarEstados", estadoRepositorio.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/editarEstado/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepositorio.findById(id);
+		
+		return cadastrar(estado.get());
+		
+	}
+	
+	@GetMapping("/removerEstado/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepositorio.findById(id);
+		estadoRepositorio.delete(estado.get());
+		
+		return listar();
+		
+	}
+	
 
 	@PostMapping("/salvarEstado")
 	public ModelAndView salvar(Estado estado, BindingResult result) {
