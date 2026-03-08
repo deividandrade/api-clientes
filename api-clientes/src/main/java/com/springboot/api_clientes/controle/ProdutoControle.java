40b5ede48eb08a09bbb589ce71f1ed70119d030b
deividandrade <deividandrade.br@gmail.com>
2026-03-07 23:25:24 -0300

    produto cadastro e lista

---
+++ api-clientes/src/main/java/com/springboot/api_clientes/controle/ProdutoControle.java
@@ -0,0 +1,63 @@
+package com.springboot.api_clientes.controle;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springboot.api_clientes.modelos.Produto;
+import com.springboot.api_clientes.repositorios.ProdutoRepositorio;
+
+@Controller
+public class ProdutoControle {
+
+	@Autowired
+	private ProdutoRepositorio produtoRepositorio;
+
+	@GetMapping("/cadastroProduto")
+	public ModelAndView cadastrar(Produto produto) {
+		ModelAndView mv = new ModelAndView("administrativo/produtos/cadastro");
+		mv.addObject("produto", produto);
+		return mv;
+	}
+	
+	@GetMapping("/listarProduto")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("administrativo/produtos/lista");
+		mv.addObject("listarProdutos", produtoRepositorio.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/editarProduto/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Produto> produto = produtoRepositorio.findById(id);
+		
+		return cadastrar(produto.get());
+		
+	}
+	
+	@GetMapping("/removerProduto/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Produto> produto = produtoRepositorio.findById(id);
+		produtoRepositorio.delete(produto.get());
+		
+		return listar();
+		
+	}
+	
+
+	@PostMapping("/salvarProduto")
+	public ModelAndView salvar(Produto produto, BindingResult result) {
+		if (result.hasErrors()) {
+			return cadastrar(produto);
+		}
+		produtoRepositorio.saveAndFlush(produto);
+		return cadastrar(new Produto());
+	}
+
+}
